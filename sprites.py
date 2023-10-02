ba7342cc174ff02bee6702d457c4ba29dfb8bc0e
wck-iipi <21dcs006@nith.ac.in>
2023-10-02 23:36:01 +0530

    Both base and engine work together

--- sprites.py
+++ sprites.py
@@ -1,10 +1,21 @@
 from settings import pygame
+#No of frames in the sprite sheet of engine
+#Bomber - 10
+#Dreadnought - 11
+#Fighter - 10
+#Frigate - 12
+#Scout - 10
+#Support_ship - 10
+#Player - 8
 
 
 def get_image(name, frame_number, width, height, scale):
-    sheet_engine = pygame.image.load(name + "engine.png").convert_alpha()
-    sheet_base = pygame.image.load(name + "base.png").convert_alpha()
+    base = pygame.image.load(name + "/base.png").convert_alpha()
+    sheet_engine = pygame.image.load(name + "/engine.png").convert_alpha()
+
     image = pygame.Surface([width, height], pygame.SRCALPHA).convert_alpha()
     image.blit(sheet_engine, (0, 0), (frame_number * width, 0, width, height))
+    image.blit(base, (0, 0))
     image = pygame.transform.scale(image, (width * scale, height * scale))
+
     return image
