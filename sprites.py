c6e14bc459609b189d22f5b24e41da041aa0d04a
wck-iipi <21dcs006@nith.ac.in>
2023-10-02 22:45:29 +0530

    Renamed assets to cleaner name

--- sprites.py
+++ sprites.py
@@ -2,8 +2,8 @@ from settings import pygame
 
 
 def get_image(name, frame_number, width, height, scale):
-    sheet_engine = pygame.image.load(name + "_engine.png").convert_alpha()
-    sheet_base = pygame.image.load(name + "_base.png").convert_alpha()
+    sheet_engine = pygame.image.load(name + "engine.png").convert_alpha()
+    sheet_base = pygame.image.load(name + "base.png").convert_alpha()
     image = pygame.Surface([width, height], pygame.SRCALPHA).convert_alpha()
     image.blit(sheet_engine, (0, 0), (frame_number * width, 0, width, height))
     image = pygame.transform.scale(image, (width * scale, height * scale))
