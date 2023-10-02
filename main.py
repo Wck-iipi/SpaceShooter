dc3d81f1246790260591e3ac7c8c159014e9fb21
wck-iipi <21dcs006@nith.ac.in>
2023-10-03 00:01:32 +0530

    Base and engine work for all ships

--- main.py
+++ main.py
@@ -5,17 +5,17 @@ from sprites import get_image
 run = True
 scroll, background_height, panels, background = initialize_background()
     
-frame_0 = get_image("./enemy/bomber", 0, 64, 64, 2)
-frame_1 = get_image("./enemy/bomber", 1, 64, 64, 2)
-frame_2 = get_image("./enemy/bomber", 2, 64, 64, 2)
-frame_3 = get_image("./enemy/bomber", 3, 64, 64, 2)
-frame_4 = get_image("./enemy/bomber", 4, 64, 64, 2)
-frame_5 = get_image("./enemy/bomber", 5, 64, 64, 2)
-frame_6 = get_image("./enemy/bomber", 6, 64, 64, 2)
-frame_7 = get_image("./enemy/bomber", 7, 64, 64, 2)
-frame_8 = get_image("./enemy/bomber", 8, 64, 64, 2)
-frame_9 = get_image("./enemy/bomber", 9, 64, 64, 2)
-frame_10 = get_image("./enemy/bomber", 10, 64, 64, 2)
+frame_0 = get_image("./enemy/bomber", 0, 2)
+frame_1 = get_image("./enemy/bomber", 1, 2)
+frame_2 = get_image("./enemy/bomber", 2, 2)
+frame_3 = get_image("./enemy/bomber", 3, 2)
+frame_4 = get_image("./enemy/bomber", 4, 2)
+frame_5 = get_image("./enemy/bomber", 5, 2)
+frame_6 = get_image("./enemy/bomber", 6, 2)
+frame_7 = get_image("./enemy/bomber", 7, 2)
+frame_8 = get_image("./enemy/bomber", 8, 2)
+frame_9 = get_image("./enemy/bomber", 9, 2)
+frame_10 = get_image("./enemy/bomber", 10, 2)
 
 while run:
     timer.tick(fps)
@@ -23,7 +23,6 @@ while run:
 
     scroll = move_background(screen, scroll, background_height, background, panels)
     screen.blit(frame_0, (0, 0))
-    screen.blit(frame_1, (500, 500))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
--- sprites.py
+++ sprites.py
@@ -1,17 +1,32 @@
 from settings import pygame
-#No of frames in the sprite sheet of engine
-#Bomber - 10
-#Dreadnought - 11
-#Fighter - 10
-#Frigate - 12
-#Scout - 10
-#Support_ship - 10
-#Player - 8
+
+# No of frames in the sprite sheet of engine
+# Bomber - 10
+# Dreadnought - 11
+# Fighter - 10
+# Frigate - 12
+# Scout - 10
+# Support_ship - 10
+# Player - 8
+
+__ship_information = {
+    "bomber": {"frames": 10, "width": 64, "height": 64},
+    "dreadnought": {"frames": 11, "width": 128, "height": 128},
+    "fighter": {"frames": 10, "width": 64, "height": 64},
+    "frigate": {"frames": 12, "width": 64, "height": 64},
+    "scout": {"frames": 10, "width": 64, "height": 64},
+    "support_ship": {"frames": 10, "width": 64, "height": 64},
+    "player": {"frames": 8, "width": 128, "height": 128},
+}
 
 
-def get_image(name, frame_number, width, height, scale):
+def get_image(name, frame_number, scale):
     base = pygame.image.load(name + "/base.png").convert_alpha()
     sheet_engine = pygame.image.load(name + "/engine.png").convert_alpha()
+
+    ship_name = name.split("/")[2]
+    width = __ship_information[ship_name]["width"]
+    height = __ship_information[ship_name]["height"]
 
     image = pygame.Surface([width, height], pygame.SRCALPHA).convert_alpha()
     image.blit(sheet_engine, (0, 0), (frame_number * width, 0, width, height))
